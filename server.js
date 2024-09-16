80558a9ddcd5206f8b8b96509323c95773c6bc52
Indira <indimorall09@gmail.com>
2024-09-16 13:11:20 +0200

    adding guide

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("First commit")
+const express  = require("express");
+const app = express();
+require("dotenv").config();
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({externded: true}));
+
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/guide", (req, res) => {
+    res.render("guide")
+})
+
+
+
+app.listen(process.env.PORT);
